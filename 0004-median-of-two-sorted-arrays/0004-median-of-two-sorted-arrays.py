18e63c5b6d2a6c5c5eed78d6836fc860af318745
limjaein <dlawodls96@naver.com>
2023-05-22 18:17:27 +0900

    Time: 81 ms (28.33%), Space: 13.4 MB (90.90%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        # 만약 홀수면 중간 idx 위치 값을 리턴
+        # 짝수면 중간 두 idx 위치 값의 평균을 리턴
+        
+        l_i = 0
+        r_i = 0
+        l = len(nums1) + len(nums2)
+        isOdd = l % 2 == 1
+        idx = l / 2
+        prev = 0
+        target = -1
+        
+        while True:
+            if r_i >= len(nums2) or (l_i < len(nums1) and nums1[l_i] < nums2[r_i]):
+                target = nums1[l_i]
+                l_i += 1
+            else:
+                target = nums2[r_i]
+                r_i += 1
+                
+            if not isOdd:                
+                # 짝수일 경우 직전 숫자 저장
+                if l_i + r_i == idx:
+                    prev += target
+                
+            if idx == l_i + r_i - 1:
+                if isOdd:
+                    return target
+                else:
+                    return (prev + target)/2.0
+                    
+                    
