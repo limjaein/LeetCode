b39d4fabfa37895f5a0f04dab3cceb9136629260
limjaein <dlawodls96@naver.com>
2023-07-19 20:52:50 +0900

    Time: 264 ms (7.17%), Space: 13.6 MB (66.22%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def binary_search(self, nums, s, e, target):
+        if e - s < 0:
+            return -1
+        
+        mid = (s + e) / 2
+        print(nums, nums[s:e+1], mid)
+        
+        # 순방향
+        print(nums[s], nums[e], nums[mid])
+        if nums[mid] == target:
+            return mid
+        
+        if nums[s] <= nums[mid]:
+            if nums[s] <= target < nums[mid]:
+                return self.binary_search(nums, 0, mid - 1, target)
+            else:
+                return self.binary_search(nums, mid + 1, e, target)
+        else:
+            print(nums[mid],target,nums[s])
+            if nums[mid] <= target < nums[s]:
+                return self.binary_search(nums, mid + 1, e, target)
+            else:
+                return self.binary_search(nums, 0, mid - 1, target)
+                
+        
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        return self.binary_search(nums, 0, len(nums) - 1, target)
